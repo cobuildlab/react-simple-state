f0d0e229126c3b291c10902747d4490a7a1a9568
Jesus Rodrguez <jesus.rodrz97@gmail.com>
2020-06-15 20:58:34 -0400

    store factory function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export * from "./hooks";
-export * from "./views";
-export {createEvent} from "./event";
+export * from './hooks';
+export * from './views';
+export { createEvent } from './event';
+export * from './core/observer';
+export * from './core/stores';
+export * from './hooks/index';
