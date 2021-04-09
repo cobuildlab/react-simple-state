cbea697e665acc8200c973322dba7dbbf52420dc
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2021-04-08 22:54:02 -0400

    add mixed event

--- src/event.ts
+++ src/event.ts
@@ -1,16 +1,12 @@
-import {
-  ConcretePublisher,
-  Publisher,
-  Subscriber,
-  Subscription,
-} from './pub-sub';
-import { CheckGeneric, Events, Reducers } from './types';
+import { ConcretePublisher, Publisher, Subscriber, Subscription } from './pub-sub';
+import { CheckGeneric, Events } from './types';
 
 export type Reducer<T, U> = (value: U, prevState: T) => T;
 
 export type EventParams<T, U> = {
   initialValue?: T | null;
   reducer?: Reducer<T, U>;
+  shared?: boolean;
 };
 
 /**
@@ -18,39 +14,43 @@ export type EventParams<T, U> = {
  */
 export class Event<T, U = unknown> {
   private value: T | null = null;
+  private sharedValue: T | null = null;
   private readonly reducer?: Reducer<T, U>;
   private publisher: Publisher<T> = new ConcretePublisher();
   private isEventEmpty = true;
+  isShared = false;
 
   constructor(eventDescriptor?: EventParams<T, U>) {
     if (eventDescriptor && eventDescriptor.initialValue) {
       this.value = eventDescriptor.initialValue;
     }
+    this.isShared = Boolean(eventDescriptor?.shared);
     this.reducer = eventDescriptor?.reducer;
   }
 
-  subscribe(
-    subscriber: (value: T | null) => void,
-    receiveLastValue = false,
-  ): Subscription {
+  subscribe(subscriber: (value: T | null) => void, receiveLastValue = false): Subscription {
     const _subscriber: Subscriber<T> = {
       update: subscriber,
     };
     if (receiveLastValue) subscriber(this.get());
     return this.publisher.subscribe(_subscriber);
   }
-  dispatch(eventValue: CheckGeneric<T, U> | null, state?: T): void {
-    const prevState = (state || this.value) as T;
+
+  dispatch(eventValue: CheckGeneric<T, U> | null): void {
+    const prevState = this.isShared ? this.sharedValue : this.value;
     const value = Object.freeze(
-      this.reducer !== null && this.reducer !== undefined
-        ? this.reducer(eventValue as U, prevState)
-        : (eventValue as T),
+      this.reducer !== null && this.reducer !== undefined ? this.reducer(eventValue as U, prevState as T) : (eventValue as T),
     );
 
     this.value = value;
     this.publisher.notify(value);
     this.isEventEmpty = false;
   }
+
+  sharedDispatch(value: T | null): void {
+    this.sharedValue = value;
+  }
+
   get(): T | null {
     return Object.freeze(this.value);
   }
@@ -72,39 +72,55 @@ export class Event<T, U = unknown> {
   }
 }
 
+export class MixedEvent<T, U, V = unknown> extends Event<T, V> {
+  events: Events<T, U>;
+
+  constructor(events: Events<T, U>, eventDescriptor?: EventParams<T, V>) {
+    super(eventDescriptor);
+    this.events = events;
+
+    this.setup();
+  }
+
+  private setup(): void {
+    const arrKeys = Object.keys(this.events) as (keyof Events<T, U>)[];
+
+    this.subscribe((value) => {
+      arrKeys.forEach((key) => {
+        const event = this.events[key];
+
+        if (!event.isShared) {
+          throw Error(`Event "${key}" must be a shared Event`);
+        }
+
+        event.sharedDispatch(value);
+      });
+    });
+
+    const callback = (v: T | null) => this.dispatch(v as CheckGeneric<T, V>);
+
+    arrKeys.forEach((key) => {
+      const event = this.events[key];
+      event.subscribe(callback);
+    });
+  }
+}
 /**
  * Creates an event from a descriptor.
  *
  * @param {EventParams} eventDescriptor - Event options.
  * @returns {Event} - Event object.
  */
-export function createEvent<T, U = T>(
-  eventDescriptor?: EventParams<T, U>,
-): Event<T, U> {
+export function createEvent<T, U = T>(eventDescriptor?: EventParams<T, U>): Event<T, U> {
   return new Event<T, U>(eventDescriptor);
 }
 
 /**
  * @template T .
- * @param {T} initalState - Initial state.
- * @param {Reducers} events - Events inputs tu create.
- * @returns {Events} - Returned events.
+ * @param {Events} events - Events to subscribe.
+ * @param {EventParams} eventDescriptor - Event options.
+ * @returns {MixedEvent} - Returned mixed event.
  */
-export function createEvents<T, U>(
-  initalState: T,
-  events: Reducers<T, U>,
-): Events<T, U> {
-  const arrKeys = Object.keys(events) as (keyof Reducers<T, U>)[];
-
-  const result = arrKeys.reduce((prev, key) => {
-    return {
-      ...prev,
-      [key]: createEvent({
-        initialValue: initalState,
-        reducer: events[key].reducer,
-      }),
-    };
-  }, {} as Events<T, U>);
-
-  return result;
+export function createMixedEvent<T, U, V = unknown>(events: Events<T, U>, eventDescriptor?: EventParams<T, V>): MixedEvent<T, U, V> {
+  return new MixedEvent(events, eventDescriptor);
 }
